a86ebaf66af84f8aab1c511dc5335eddfa039871
Grant Smith <gss1@iu.edu>
2022-09-14 14:48:02 -0400

    Working on movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+
+    public float moveSpeed = 5f;
+
+    Color lerpedColor = Color.blue;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float input = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(input * moveSpeed, rb.velocity.y);
+
+
+        lerpedColor = Color.Lerp(Color.blue, Color.red, GetComponent<Rigidbody>().velocity.magnitude / 30);
+        gameObject.GetComponent<SpriteRenderer>().material.color = lerpedColor;
+
+
     }
 }
