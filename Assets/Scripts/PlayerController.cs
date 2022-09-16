d960267755885ab1a18b23c241bb8b46839a4d66
GrrantSmith121 <gss1@iu.edu>
2022-09-15 21:21:31 -0400

    Failed at movement systemm

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D rb;
 
-    public float moveSpeed = 5f;
+    public float moveSpeed;
+    public float jumpForce;
+    private bool isJumping;
+    private float moveHorizontal;
+    private float moveVertical;
 
     Color lerpedColor = Color.blue;
 
@@ -16,18 +21,34 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+
+        moveSpeed = 3f;
+        jumpForce = 60f;
+        isJumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float input = Input.GetAxisRaw("Horizontal");
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+       
+        /*
         rb.velocity = new Vector2(input * moveSpeed, rb.velocity.y);
 
 
         lerpedColor = Color.Lerp(Color.blue, Color.red, GetComponent<Rigidbody>().velocity.magnitude / 30);
         gameObject.GetComponent<SpriteRenderer>().material.color = lerpedColor;
+        */
 
+    }
 
+    void FixedUpdate() 
+    { 
+        if(moveHorizontal > 0.1f || moveHorizontal < -0.1f) 
+        {
+            rb.AddForce(new Vector2(moveHorizontal * moveSpeed, 0f), ForceMode2D.Impulse);
+        }
+        print(moveHorizontal);
     }
 }
